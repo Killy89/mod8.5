140958e315292e4741736dfc3023d03b297890d4
Michał Marciniak <marciniak.michal89@gmail.com>
2017-11-22 19:17:15 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Podaj a: '),
+    h = prompt('Podaj h: ');
+var triangleArea = a*h/2;
+
+alert('Pole trójkąta to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
